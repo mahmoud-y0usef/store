b15729877e8e044d603e48dcddf06ed163e3f3e6
mahmoud-y0usef <mahmoudyousef59@outlook.com>
2023-05-10 23:50:40 +0300

    update

---
+++ js/app.js
@@ -0,0 +1,50 @@
+// navbar
+var bool = true;
+function menu(){
+  if (bool == true) {
+    let menu = document.getElementsByClassName("nav-bar-menu");
+    for (var i=0;i<menu.length;i+=1){
+      menu[i].style.display = 'block';
+    }
+    bool = false;
+    
+  }else{
+    let menu = document.getElementsByClassName("nav-bar-menu");
+    for (var i=0;i<menu.length;i+=1){
+      menu[i].style.display = 'none';
+    }
+    bool = true;
+  }
+
+}
+
+
+
+$(".nommenu").hide();
+$(".men").click(function() {
+  $(".nommenu").show();
+})
+
+// slider
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+
+
